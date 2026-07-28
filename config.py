841bf9f9478ff3db0313fd7b92210c8ccefa42af
zxzkhkh1 <zxzkhkh1@atomicmail.io>
2026-07-29 00:08:46 +0330

    Add configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+PORT = int(os.environ.get("PORT", 20190))
+
+MODES = {
+    "classic": False,
+    "secure": True,
+    "tls": False
+}
+
+USERS = {
+    "tg": "a9d4f2b8e1c74109852317f0e632b5d8"
+}
+
+AD_TAG = "9dad4a7bcba8796a2415dd8a3627396c"
